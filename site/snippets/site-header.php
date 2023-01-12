36c62a65e97cc5c019712ef3a0e69b507fd9d47a
Vincent <rouleau.vincent@gmail.com>
2023-01-12 15:12:37 -0500

    WIP header

--- site/snippets/site-header.php
+++ site/snippets/site-header.php
@@ -1,12 +1,6 @@
-<header id="site-header">
-    <div class="navbar">
-        <a class="logo" href="<?= $site->url() ?>" aria-label="Site logo">
-            <?= asset('assets/svg/calotte.svg')->read() ?>
-        </a>
-        <button class="button portrait" title="Menu" aria-haspopup="true" aria-controls="menu">Menu</button>
-        <div class="status landscape-flex">
-            <?= snippet('time-of-day', ['id'=>'navbar-time-of-day']) ?>
-            <a class="button" href="mailto:<?= $site->contact_email() ?>"><?= $site->contact_email() ?></a>
-        </div>
-    </div>
+<header id="header">
+    <nav>
+        <a href="<?= $site->url() ?>"><?= $site->title() ?></a>
+        <button class="button" title="Menu" aria-haspopup="true" aria-controls="menu">Menu</button>
+    </nav>
 </header>
--- site/templates/default.php
+++ site/templates/default.php
@@ -1,11 +1,10 @@
-<?= snippet('header') ?>
+<?= snippet('site-start') ?>
+<?= snippet('site-header') ?>
 <div id="page" class="default">
-    <?php if (!$page->isHomePage()): ?>
-    <?= snippet('crumbs') ?>
-    <?= snippet('blocks/landing-hr') ?>
-    <?php endif ?> 
+    <h1><?= $page->title() ?></h1>
     <?php foreach ($page->blocks()->toBlocks() as $block): ?>
-        <?php snippet('blocks/' . $block->type(), ['block'=>$block]) ?>
+    <?php snippet('blocks/' . $block->type(), ['block'=>$block]) ?>
     <?php endforeach ?>
 </div>
 <?= snippet('footer') ?>
+<?= snippet('site-end') ?>
