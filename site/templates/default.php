26291c221c9b29143d5f317096d145188507f85f
Vincent <rouleau.vincent@gmail.com>
2023-01-18 07:02:47 -0500

    playing around

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,6 +1,6 @@
 <?= snippet('site-start') ?>
 <?= snippet('site-header') ?>
-<div id="page" class="default">
+<div id="page" class="default ctn">
     <h1><?= $page->title() ?></h1>
     <?php foreach ($page->blocks()->toBlocks() as $block): ?>
     <?php snippet('blocks/' . $block->type(), ['block'=>$block]) ?>
