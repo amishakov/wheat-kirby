bc8e04a27c0b20ee0baf66fd266366c4ef659bf0
hotdoy <rouleau.vincent@gmail.com>
2023-01-27 19:43:52 -0500

    Update img.php

--- site/snippets/img.php
+++ site/snippets/img.php
@@ -1,11 +1,8 @@
-<?php
-$sizes = "(min-width: 1200px) 25vw, (min-width: 900px) 33vw, (min-width: 600px) 50vw, 100vw";
-if ($image): ?>
+<?php if ($image): ?>
 <img
     alt="<?= $image->alt() ?>"
     src="<?= $image->resize(300)->url() ?>"
     srcset="<?= $image->srcset('webp') ?>"
-    sizes="<?= $sizes ?>"
     width="<?= $image->resize(1800)->width() ?>"
     height="<?= $image->resize(1800)->height() ?>"
     loading="lazy"
