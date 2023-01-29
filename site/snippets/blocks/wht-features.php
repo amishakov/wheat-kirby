1e780424e0af421ae8344220d46104857604676b
hotdoy <rouleau.vincent@gmail.com>
2023-01-29 14:07:03 -0500

    features

---
+++ site/snippets/blocks/wht-features.php
@@ -0,0 +1,15 @@
+<wht-features class="block">
+    <div class="ctn">
+        <?php foreach ($block->items()->toStructure() as $item): ?>
+        <article>
+            <?php if ($picture = $item->picture()->toFile()): ?>
+            <figure class="mb-100 <?= $block->size()->or('icon') ?>">
+            <?= snippet('img', ['image'=>$picture]) ?>
+            </figure>
+            <?php endif ?>
+            <h3 class="h3"><?= $item->title() ?></h3>
+            <p><?= $item->text() ?></p>
+        </article>
+        <?php endforeach ?>
+    </div>
+</wht-features>
