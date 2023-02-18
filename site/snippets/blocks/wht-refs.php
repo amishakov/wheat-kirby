8174f3f9f80ad5f1a3599a4d4dd4ca9bc008d447
hotdoy <rouleau.vincent@gmail.com>
2023-02-17 19:19:42 -0500

    no more links for this one

--- site/snippets/blocks/wht-refs.php
+++ site/snippets/blocks/wht-refs.php
@@ -4,13 +4,6 @@
             <div class="prose">
                 <?= $block->text()->kirbytext() ?>
             </div>
-            <?php if ($block->links()->isNotEmpty()): ?>
-            <div class="links mt-200">
-                <?php foreach ($block->links()->toStructure() as $l): ?>
-                <a class="<?= $s = $l->style()->or('btn') ?>" href="<?= $l->url() ?>"><?= $l->label() ?></a>
-            </div>
-            <?php endforeach ?>
-            <?php endif ?>
         </div>
         <ul class="refs">
             <?php foreach ($block->items()->toStructure() as $item): ?>
