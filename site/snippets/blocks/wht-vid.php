fae52109372a3403510d9d5da62ece196495363d
hotdoy <rouleau.vincent@gmail.com>
2023-03-04 09:10:59 -0500

    whitespace

--- site/snippets/blocks/wht-vid.php
+++ site/snippets/blocks/wht-vid.php
@@ -1,11 +1,9 @@
 <wht-vid class="block">
     <div class="ctn">
-        <?php if ($vid = $block->video()->toFile()): ?>
-
+        <?php if ($v = $block->video()->toFile()): ?>
         <?php $p = $block->poster()->isNotEmpty() ? $block->poster()->toFile()->url() : "" ?>
-
         <video poster="<?= $p ?>" autoplay loop muted playsinline disableremoteplayback>
-            <source type="video/mp4" src="<?= $vid->url() ?>">
+            <source type="video/mp4" src="<?= $v->url() ?>">
 	    </video>
         <?php endif ?>
     </div>
