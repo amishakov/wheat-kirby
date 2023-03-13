94787bc9b530d535ca8794e3e6a17f2eace2b03e
hotdoy <rouleau.vincent@gmail.com>
2023-03-13 07:57:56 -0400

    working on lazy stuff

--- site/snippets/img.php
+++ site/snippets/img.php
@@ -1,13 +1,12 @@
 <?php if ($image): ?>
-<figure style="--aspect-ratio:<?= $image->width() . '/' . $image->height() ?>;" is="lazy-img">
+<lazy-img style="--aspect-ratio:<?= $image->width() . '/' . $image->height() ?>;">
     <img
         alt="<?= $image->alt() ?>"
         src="<?= $image->resize(300)->url() ?>"
         srcset="<?= $image->srcset('webp') ?>"
         width="<?= $image->width() ?>"
         height="<?= $image->height() ?>"
-        loading="lazy"
-    >
-    <img-ph></img-ph>
-</figure>
+        loading="lazy">
+    <lazy-img-ph></lazy-img-ph>
+</lazy-img>
 <?php endif ?>
