674423af052210f018de777235809c0d78f2312b
Vincent <rouleau.vincent@gmail.com>
2023-04-20 12:02:42 -0400

    pages now uses slots

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,5 +1,7 @@
-<?= snippet('site-start') ?>
-<?php foreach ($page->blocks()->toBlocks() as $block): ?>
-<?php snippet('blocks/' . $block->type(), ['block'=>$block]) ?>
-<?php endforeach ?>   
-<?= snippet('site-end') ?>
+<?php snippet('document', slots: true) ?>
+<?php slot() ?>
+    <?php foreach ($page->blocks()->toBlocks() as $block): ?>
+    <?php snippet('blocks/' . $block->type(), ['block'=>$block]) ?>
+    <?php endforeach ?>   
+<?php endslot() ?>
+<?php endsnippet() ?>
