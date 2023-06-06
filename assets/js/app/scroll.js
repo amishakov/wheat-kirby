bc4a497ebf670e7786c7e5a8ea6940807348c93a
Vincent <rouleau.vincent@gmail.com>
2023-06-06 09:33:54 -0400

    Update scroll.js

--- assets/js/app/scroll.js
+++ assets/js/app/scroll.js
@@ -21,18 +21,40 @@
 		}
 		lastScroll = currentScroll;
 	});
+	document.addEventListener("htmx:afterSwap", function (event) {
+		if (event.detail.boosted) {
+			scrollToTop("auto");
+		}
+	});
 })();
 
 function scrollOn() {
-	document.body.style.overflow = 'auto';
+	document.body.style.removeProperty("overflow");
+	document.body.style.removeProperty("padding-right");
 }
 
 function scrollOff() {
-	document.body.style.overflow = 'hidden';
+	document.body.style.overflow = "hidden";
+	let userAgent = window.navigator.userAgent.toLowerCase(),
+	macosPlatforms = /(macintosh|macintel|macppc|mac68k|macos)/i,
+	windowsPlatforms = /(win32|win64|windows|wince)/i,
+	iosPlatforms = /(iphone|ipad|ipod)/i,
+	os = null;
+	if (macosPlatforms.test(userAgent)) {
+		// Nothing
+	} else if (iosPlatforms.test(userAgent)) {
+		// Nothing
+	} else if (windowsPlatforms.test(userAgent)) {
+		document.body.style.paddingRight = "17px";
+	} else if (/android/.test(userAgent)) {
+		// Nothing
+	} else if (!os && /linux/.test(userAgent)) {
+		// Nothing
+	}
 }
 
-function scrollToTop() {
-	window.scrollTo({ top: 0, behavior: "smooth" });
+function scrollToTop(behavior = "smooth") {
+	window.scrollTo({ top: 0, behavior: behavior });
 }
 
 document.body.addEventListener("htmx:load", (event) => {
