62d29676d0529e872f0624902d1f430414b59f1a
Vincent <rouleau.vincent@gmail.com>
2023-06-15 15:26:30 -0400

    basic search engine

--- site/snippets/search-results.php
+++ site/snippets/search-results.php
@@ -2,11 +2,15 @@
 <?php if ($results->count() > 0): ?>
 <?php foreach ($results->listed() as $result): ?>
 <article>
-    <a href="<?= $result->url() ?>"><?= $result->title() ?></a>
+    <a href="<?= $result->url() ?>">
+        <div class="title"><?= $result->title() ?></div>
+        <div class="desc"><?= $result->seoDescription() ?></div>
+        <div class="path"><?= $result->url() ?></div>
+    </a>
 </article>
 <?php endforeach ?>
 <?php else: ?>
 <article>
-    [no results]
+    <div class="title">[no results]</div>
 </article>
 <?php endif ?>
