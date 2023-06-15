ea05e842875350730a82b2c5b2a1e3882c029f0d
hotdoy <rouleau.vincent@gmail.com>
2023-06-15 09:27:29 -0400

    new build and test set

--- site/snippets/assets.php
+++ site/snippets/assets.php
@@ -8,18 +8,20 @@
     };
 ?>
 
-<style><?= asset('assets/css/site-loading.css')->read() ?></style>
-
 <link rel="preload" href="/assets/fonts/inter-v12-latin-regular.woff2" as="font" type="font/woff2" crossorigin="">
 <link rel="preload" href="/assets/fonts/ibm-plex-serif-v19-latin-regular.woff2" as="font" type="font/woff2" crossorigin="">
 
 <?php if ($dev = Cookie::exists('dev')): ?>
 <link rel="stylesheet" href="/assets/css/index.css?v=<?= $salt ?>">
+<?php foreach (array_slice(scandir('assets/js/app'), 2) as $f): ?>
+<script src="/assets/js/app/<?= $f ?>?v=<?= $salt ?>" defer></script>
+<?php endforeach ?>
+<?php foreach (array_slice(scandir('assets/js/com'), 2) as $f): ?>
+<script src="/assets/js/com/<?= $f ?>?v=<?= $salt ?>" defer></script>
+<?php endforeach ?>
+
 <?php else: ?>
 <link rel="stylesheet" href="/assets/css/app.css?v=<?= $salt ?>">
+<script src="/assets/js/app.js?v=<?= $salt ?>" defer></script>
+<script src="/assets/js/com.js?v=<?= $salt ?>" defer></script>
 <?php endif ?>
-
-<script src="/assets/js/app/app.js?v=<?= $salt ?>" defer></script>
-<script src="/assets/js/com/site-menu.js?v=<?= $salt ?>" defer></script>
-<script src="/assets/js/com/site-search.js?v=<?= $salt ?>" defer></script>
-<script src="/assets/js/com/carousel.js?v=<?= $salt ?>" defer></script>
